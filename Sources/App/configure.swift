5f8034478644b999d3730fb8bc67251ea9ef0d40
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2020-10-05 06:20:19 +0900

    debug

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 public func configure(_ app: Application) throws {
-    app.http.server.configuration.port = 3000
+    app.http.server.configuration.port = 80
     app.http.server.configuration.supportPipelining = true
     app.http.server.configuration.requestDecompression = .enabled
     app.http.server.configuration.responseCompression = .enabled
