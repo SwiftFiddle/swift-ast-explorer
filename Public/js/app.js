353363b30b6fdb12a254efb3246ee96d89a9ae7c
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-08-11 13:42:01 +0900

    Add sentry.io logger

--- Public/js/app.js
+++ Public/js/app.js
@@ -2,6 +2,12 @@
 
 import { SwiftFormat } from "./swift_format.js";
 
+Sentry.init({
+  dsn: "https://27b9f9a7ec6f46a6ac6ce4445a6e6783@o938512.ingest.sentry.io/5899228",
+  integrations: [new Sentry.Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
+
 ace.require("ace/ext/language_tools");
 const Range = ace.require("ace/range").Range;
 
