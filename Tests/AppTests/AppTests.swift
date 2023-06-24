519439b41393d6d24389291e311651d98ebc9cfc
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-06-25 02:17:18 +0900

    Sort test data

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -273,7 +273,6 @@ final class AppTests: XCTestCase {
     )
 
     let decoder = JSONDecoder()
-    print(response.syntaxJSON)
 
     XCTAssertEqual(
       try decoder.decode([TreeNode].self, from: Data(response.syntaxJSON.utf8)),
