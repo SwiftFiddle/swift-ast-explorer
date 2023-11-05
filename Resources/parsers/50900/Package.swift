8c2cd03a9666ef5f023911e35babe641483cd21a
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2023-11-05 11:21:25 +0900

    Update SwiftSyntax 509.0.2

--- Resources/parsers/50900/Package.swift
+++ Resources/parsers/50900/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     .macOS(.v13)
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.1"),
+    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.2"),
   ],
   targets: [
     .executableTarget(
