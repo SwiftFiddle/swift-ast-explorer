9a2c3f1cdfd1d8ed60036d1f403c3ab88b7dd049
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2024-03-04 17:30:25 +0900

    Add Swift Syntax 510.0.0

--- build_pasers.sh
+++ build_pasers.sh
@@ -2,4 +2,5 @@
 
 swift build -c release --package-path Resources/parsers/50800
 swift build -c release --package-path Resources/parsers/50900
+swift build -c release --package-path Resources/parsers/51000
 swift build -c release --package-path Resources/parsers/trunk
